a35b15af4cbcd7d23e2754607d01f3cea85634ef
rishitsaiya <rishitsaiya@gmail.com>
2020-12-26 21:26:18 +0530

    Added Lab Assignment 3

---
+++ Assignment-3/lab3.sql
@@ -0,0 +1,19 @@
+INSERT INTO part VALUES (1, 'Part1', 'Red', 4);
+INSERT INTO part VALUES (2, 'Part2', 'Green', 5);
+INSERT INTO part VALUES (3, 'Part3', 'Blue', 6);
+
+INSERT INTO supplier VALUES (1, 'Supplier1', 'Mumbai', 'SBI');
+INSERT INTO supplier VALUES (2, 'Supplier2', 'Chennai', 'Kotak');
+
+INSERT INTO shipment VALUES (1, 1, 1, '2020-09-19', 4, 10.0);
+INSERT INTO shipment VALUES (2, 1, 2, '2020-08-19', 5, 20.0);
+INSERT INTO shipment VALUES (3, 2, 1, '2020-07-19', 6, 30.0);
+INSERT INTO shipment VALUES (4, 2, 2, '2020-09-19', 7, 40.0);
+INSERT INTO shipment VALUES (5, 3, 1, '2020-08-19', 8, 50.0);
+INSERT INTO shipment VALUES (6, 3, 2, '2020-07-19', 9, 60.0);
+INSERT INTO shipment VALUES (7, 1, 1, '2020-09-19', 1, 70.0);
+INSERT INTO shipment VALUES (8, 1, 2, '2020-08-19', 2, 80.0);
+INSERT INTO shipment VALUES (9, 2, 1, '2020-07-19', 3, 90.0);
+INSERT INTO shipment VALUES (10, 2, 2, '2020-09-19', 4, 10.0);
+INSERT INTO shipment VALUES (11, 3, 1, '2020-08-19', 5, 20.0);
+INSERT INTO shipment VALUES (12, 3, 2, '2020-07-19', 6, 30.0);
